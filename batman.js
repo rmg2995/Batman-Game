bf74d140db994964a76a00e06a071a2c23adf7c6
Tzikas <niko.tzikas@gmail.com>
2020-06-23 16:14:23 -0400

    refactor

---
+++ batman.js
@@ -0,0 +1,175 @@
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+canvas.width = 700//window.innerWidth /1.2
+canvas.height = 500//window.innerHeight /1.2
+
+
+//Game class
+class Game {
+
+    constructor(){
+        this.image = new Image()
+        this.image.src = "/Batman/batman experiment.gif"
+        this.batrangs = []
+    }
+
+
+    drawMap = () => {
+        ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height )
+    }
+
+
+
+}
+let game = new Game()
+
+
+
+class Character{
+    constructor(name, src, xCanvas, yCanvas){
+        this.image = new Image()
+        this.image.src =  src
+        this.x = 0
+        this.y = 0
+        this.width = 45
+        this.height = 50
+        this.xCanvas = xCanvas
+        this.yCanvas = yCanvas
+        this.widthCanvas = 64
+        this.heightCanvas = 64
+        this.batrangs = []  //objects 
+        this.bangGunBullets = [] //objects 
+        this.direction = "Up"
+    }
+
+    drawCharacter = () => {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height, this.xCanvas, this.yCanvas, this.widthCanvas, this.heightCanvas) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
+        this.batrangs.forEach(rang => rang.drawThisBatrang() )
+    }
+
+    shootBatrang = () => {
+        this.batrangs.push( 
+            new Batrang(this.xCanvas, this.yCanvas, 50, 50, 'blue', this.direction, this.batrangs.length-1) 
+        )
+    }
+    
+}
+
+
+class Batman extends Character{
+    constructor(name, image, x, y, width, height, xCanvas, yCanvas, widthCanvas, heightCanvas, batrangs){
+        super(name, image, x, y, width, height, xCanvas, yCanvas, widthCanvas, heightCanvas, batrangs) 
+        this.name = name;
+        
+    }
+
+
+
+}
+
+
+const batman = new Batman("Batman","/Batman/batman finally.jpg", 300, 250)
+
+
+
+
+class Joker extends Character{
+    constructor(name, image, x, y, width, height, xCanvas, yCanvas, widthCanvas, heightCanvas){
+        super(name, image, x, y, width, height, xCanvas, yCanvas, widthCanvas, heightCanvas) 
+        this.name = name;
+    }
+
+
+}
+
+
+//THis will probably end up going in an array 
+
+const joker = new Joker("Joker","/Batman/Joker-01.png", 50 ,100)
+
+const joker2 = new Joker("Joker","/Batman/Joker-01.png", 350 ,100)
+
+
+
+
+class Batrang {
+    constructor(x,y,width, height,color, direction, index){
+        this.x = x;
+        this.y = y; 
+        this.width = width;
+        this.height = height;
+        this.color = color; 
+        this.direction = direction,
+        this.index = index
+    }
+    drawThisBatrang = () => {
+        switch(this.direction){
+            case 'Up':
+                ctx.fillRect(this.x, this.y--, this.width, this.height, this.color)
+                break;
+            case 'Down':
+                ctx.fillRect(this.x, this.y++, this.width, this.height, this.color)
+                break;
+            case 'Left':
+                ctx.fillRect(this.x--, this.y, this.width, this.height, this.color)
+                break;
+            case 'Right':
+                ctx.fillRect(this.x++, this.y, this.width, this.height, this.color)
+                break;                                                
+        }
+        if(this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height){
+            batman.batrangs.splice(this.index,1)
+        }
+
+    }
+}
+
+
+
+function animationLoop() {
+    animationID = window.requestAnimationFrame(animationLoop)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // ctx.fillRect(0, 0, canvas.width, canvas.height)
+    game.drawMap()
+    batman.drawCharacter()
+    joker.drawCharacter()
+    joker2.drawCharacter()
+    // ctx.drawImage(batman.image, batman.xImage, batman.yImage, batman.widthImage,batman.heightImage, batman.xCanvas, batman.yCanvas, batman.widthCanvas, batman.heightCanvas) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
+    // game.draw()
+    // ctx.drawImage(joker.image, joker.xImage, joker.yImage, joker.widthImage,joker.heightImage, joker.xCanvas, joker.yCanvas, joker.widthCanvas, joker.heightCanvas)
+}
+
+animationLoop()
+document.addEventListener('keydown', function(event){
+    switch(event.key) {
+        case 'ArrowUp':
+            batman.y = 48* 3
+            batman.yCanvas -= 10
+            batman.x = (batman.x + 45) % 90
+            batman.direction= "Up"
+            break;
+        case 'ArrowDown':
+            batman.y =0
+            batman.yCanvas += 10
+            batman.x = (batman.x + 45) % 90
+            batman.direction = "Down"
+            break;
+        case 'ArrowLeft':
+            
+            batman.y =48* 1
+            batman.xCanvas -= 10
+            batman.x = (batman.x + 45) % 90
+            batman.direction= "Left"
+            break;
+        case 'ArrowRight':
+            batman.y = 48 * 2
+            batman.xCanvas += 10
+            batman.x = (batman.x + 45) % 90
+            batman.direction = "Right"
+            break;
+        case " ":
+            console.log()
+            batman.shootBatrang()
+    }
+})
