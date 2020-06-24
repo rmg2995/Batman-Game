3bc3f21e7adc01e82e559f3ff16d179bab56e3fc
Roy Gutierrez <roymg29@gmail.com>
2020-06-24 17:54:55 -0400

    added score, background, and start game

--- batman.js
+++ batman.js
@@ -1,34 +1,43 @@
 
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-canvas.width = 700//window.innerWidth /1.2
-canvas.height = 500//window.innerHeight /1.2
+canvas.width = 600//window.innerWidth /1.2
+canvas.height = 400//window.innerHeight /1.2
+let startGame = document.querySelector("#startbtn")
+startGame.onclick = ()=> {
+    game.addJoker()
+    animationLoop()
+}
 
 
 //Game class
 class Game {
 
     constructor(){
+        
         this.image = new Image()
         this.image.src = "Batman/batman experiment.gif"
         // this.batrangs = []
         this.jokers = []
+        
+        this.score = 0
     }
     addJoker = () => {
         // const joker = new Joker("Joker","/Batman/Joker-01.png", 50 ,100)
         // const joker2 = new Joker("Joker","/Batman/Joker-01.png", 350 ,100)
         // this.jokers.push(joker, joker2)
+        
         setInterval(() => {
-            let joker = new Joker("Joker","Batman/Joker-01.png", Math.random() * canvas.width , Math.random() * canvas.height)
+            let joker = new Joker("Joker","Batman/Joker-01.png", Math.random() * canvas.width , 0)
             this.jokers.push(joker)
-        }, 2000)
+        }, 800)
     }
 
     drawMap = () => {
         ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height )
     }
 
-
+    
 
 }
 let game = new Game()
@@ -45,8 +54,8 @@ class Character{
         this.height = 50
         this.xCanvas = xCanvas
         this.yCanvas = yCanvas
-        this.widthCanvas = 64
-        this.heightCanvas = 64
+        this.widthCanvas = 45
+        this.heightCanvas = 50
         this.batrangs = []  //objects 
         this.bangGunBullets = [] //objects 
         this.direction = "Down"
@@ -64,20 +73,27 @@ class Character{
 
         game.jokers.forEach((joker, i) => {
             
-            //joker.moveJoker()
-            //console. log(joker)
-            //this.batrangs.forEach(batrang => {
-            if (joker.xCanvas < batrang.x + batrang.width &&
-                joker.xCanvas + joker.widthCanvas > batrang.x &&
-                joker.yCanvas < batrang.y + batrang.height &&
-                joker.yCanvas + joker.heightCanvas > batrang.y) {
-                     game.jokers.splice(i,1)
+        //joker.moveJoker()
+        //console. log(joker)
+        //this.batrangs.forEach(batrang => {
+        if (joker.xCanvas < batrang.x + batrang.width &&
+            joker.xCanvas + joker.widthCanvas > batrang.x &&
+            joker.yCanvas < batrang.y + batrang.height &&
+            joker.yCanvas + joker.heightCanvas > batrang.y) {
+                game.jokers.splice(i,1)
+                document.querySelector('.score span').innerHTML++
                         
 
+            }
+            
+            if (joker.xCanvas < batman.xCanvas + batman.widthCanvas &&
+                joker.xCanvas + joker.widthCanvas > batman.xCanvas &&
+                joker.yCanvas < batman.yCanvas + batman.heightCanvas &&
+                joker.yCanvas + joker.heightCanvas > batman.yCanvas) {
+                    window.cancelAnimationFrame(animationID)
                     //console.log('collision detected!')
-                }
-
-
+                    //  alert('game over')
+            }  
             
 
 
@@ -85,8 +101,7 @@ class Character{
        
             
     } 
-
-
+    
         
     drawCharacter = () => {
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height, this.xCanvas, this.yCanvas, this.widthCanvas, this.heightCanvas) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
@@ -117,7 +132,7 @@ class Batman extends Character{
 }
 
 
-const batman = new Batman("Batman","Batman/batman finally.jpg", 300, 250)
+const batman = new Batman("Batman","Batman/batman finally.jpg", 225, 150)
 
 
 
@@ -129,8 +144,9 @@ class Joker extends Character{
     
     }
     moveJoker= () => {
-        this.xCanvas++ 
+        
         this.yCanvas++
+        
         
     }
 
@@ -175,13 +191,13 @@ class Batrang {
                 break;                                                
         }
         if(this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height){
-            batman.batrangs.splice(this.index,0)
+            batman.batrangs.splice(this.index,1)
         }
 
     }
 }
 
-game.addJoker()
+
 
 function animationLoop() {
     animationID = window.requestAnimationFrame(animationLoop)
@@ -190,13 +206,20 @@ function animationLoop() {
     game.drawMap()
     batman.drawCharacter()
     game.jokers.forEach(joker => joker.drawCharacter())
+    game.jokers.forEach(joker => joker.moveJoker())
+
     
+    
+        
+       
+    
+      
     // ctx.drawImage(batman.image, batman.xImage, batman.yImage, batman.widthImage,batman.heightImage, batman.xCanvas, batman.yCanvas, batman.widthCanvas, batman.heightCanvas) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
     // game.draw()
     // ctx.drawImage(joker.image, joker.xImage, joker.yImage, joker.widthImage,joker.heightImage, joker.xCanvas, joker.yCanvas, joker.widthCanvas, joker.heightCanvas)
 }
 
-animationLoop()
+ //animationLoop()
 document.addEventListener('keydown', function(event){
     switch(event.key) {
         case 'ArrowUp':
